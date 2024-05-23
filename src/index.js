245fb2d1d532af72f7a60cb2f408eda5cc7df13a
Alan Shaw <alan.shaw@protocol.ai>
2024-05-23 09:59:44 +0100

    feat: export ipld multicodec code for CAR

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 export { CARReaderStream } from './reader.js'
 export { CARWriterStream } from './writer.js'
+
+/**
+ * IPLD multicodec code for CAR.
+ * @see https://github.com/multiformats/multicodec/blob/c954a787dc6a17d099653e5f90d26fbd177d2074/table.csv#L141
+ */
+export const code = 0x0202
